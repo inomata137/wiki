f9910daf615c37f03bf3267d095f820aa853a6b1
inomata137 <inomata-kazuki137@g.ecc.u-tokyo.ac.jp>
2021-12-13 03:57:28 +0900

    modified directory structure

--- html.ts
+++
@@ -1,9 +0,0 @@
-import mdit from 'markdown-it'
-import fs from 'fs'
-const md = mdit()
-const text = fs.readFileSync('./sample.md', 'utf-8').toString()
-const result = md.render(text)
-if (!fs.existsSync('./output')) {
-  fs.mkdirSync('./output')
-}
-fs.writeFileSync('./output/output.html', result)
--- src/translate.ts
+++ src/translate.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import rewrite from './rewriter'
 
 const text = fs.readFileSync("./sample.md", 'utf8').toString()
-if (!fs.existsSync('./output')) {
-  fs.mkdirSync('./output')
+if (!fs.existsSync('../output')) {
+  fs.mkdirSync('../output')
 }
-fs.writeFileSync('./output/output.txt', rewrite(text))
+fs.writeFileSync('../output/output.txt', rewrite(text))
